462d1d633d8ad7ea7db81ea42788fdce796521ca
krupabt <167057503+krupabt@users.noreply.github.com>
2025-09-10 08:23:45 +0530

    Create products.js

---
+++ phase1/src/data/products.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    description: "High-quality wireless headphones with noise cancellation and long battery life.",
+    price: 699,
+    image: "Wireless Headphones.webp",
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    description: "Stylish smartwatch with fitness tracking, heart rate monitor, and notifications.",
+    price: 599,
+    image: "Smart Watch.jpg",
+  },
+  {
+    id: 3,
+    name: "Bluetooth Speaker",
+    description: "Portable Bluetooth speaker with deep bass and 12 hours of playtime.",
+    price: 999.99,
+    image: "Bluetooth Speaker.jpg",
+  },
+  {
+    id: 4,
+    name: "Digital Camera",
+    description: "Compact digital camera with 20MP resolution and 4K video recording.",
+    price: 2500.99,
+    image: "Digital Camera.jpg",
+  },
+  {
+    id: 5,
+    name: "Gaming Mouse",
+    description: "Ergonomic gaming mouse with customizable buttons and RGB lighting.",
+    price: 490.99,
+    image: "Gaming Mouse.webp",
+  },
+  {
+    id: 6,
+    name: "Laptop Backpack",
+    description: "Durable and water-resistant backpack with padded laptop compartment.",
+    price: 1999.99,
+    image: "Laptop Backpack.webp",
+  },
+];
+
+export default products;
